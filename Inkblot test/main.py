0ee75fc66cba39dd0f9bac4bd0db57797d492537
Pulkit Garg <garg.decipher@gmail.com>
2018-11-17 12:18:09 +0530

    v1

--- Inkblot test/main.py
+++ Inkblot test/main.py
@@ -1,9 +1,11 @@
-from selenium import webdriver
+from flask import Flask,request,abort
+app = Flask(__name__)
 
+from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-
+import json
 
 
 
@@ -11,51 +13,65 @@ from selenium.webdriver.support import expected_conditions as EC
 
 # =====================================================================================================
 
-driver_path='./chromedriver.exe'
 
+driver_path='./bin/chromedriver.exe'
 
 chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--headless')
 chrome_options.add_argument('--no-sandbox') 
-
 driver = webdriver.Chrome(executable_path=driver_path, chrome_options=chrome_options)
-driver.get('http://theinkblot.com/') 
-
-print(driver.title)
-
 d=driver
-d.find_element_by_class_name('gbutton').click()
+
+def initialize():
+    d.get('http://theinkblot.com/') 
+    print(driver.title)
+    d.find_element_by_class_name('gbutton').click()
+
+
+
 
 # =====================================================================================================
 
-x=[]
 
 
-for i in range(9):
+def getQues():
+    initialize()
+    x=[]
+    for i in range(9):
 
-    ques=[]
-    a=d.find_element_by_class_name('ttable').find_elements_by_tag_name('tr')
-    for n in a:
-        fix=n.text.encode('utf-8','ignore')
-        print(fix)
-        ques.append(fix)
-
-
-    print(a)
-    print('..........................................................')
-    l=d.find_elements_by_name('sel')
-    l[0].click()
-    d.find_element_by_class_name('gbutton').click()
-    img=d.find_element_by_class_name('plate').get_attribute('src')
-    
-    x.append({"step":i,"ink":img,"content":ques})
+        ques=[]
+        a=d.find_element_by_class_name('ttable').find_elements_by_tag_name('tr')
+        for n in a:
+            fix=n.text.encode('utf-8','ignore')
+            ques.append(fix)
 
 
+        print(a)
+        print('..........................................................')
+        l=d.find_elements_by_name('sel')
+        l[0].click()
+        d.find_element_by_class_name('gbutton').click()
+        img=d.find_element_by_class_name('plate').get_attribute('src')
+        
+        x.append({"step":i,"ink":img,"content":ques})
+    return json.dumps(x)
 
-import json
-js=json.dumps(x)
 
-print(js)
+
+
+
+
+
+
+
+
+
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 # for chkbox in l:
 #     chkbox.click()
@@ -67,3 +83,51 @@ print(js)
 #     )
 # finally:
 #     d.quit()
+
+
+
+
+
+
+
+def getRes(ans):
+    initialize()
+    for i in ans:
+        ques=[]
+        a=d.find_element_by_class_name('ttable').find_elements_by_tag_name('input')
+        a[i].click()
+        print(d.title)
+        print('..........................................................')
+        
+        d.find_element_by_class_name('gbutton').click()
+    
+    print('..........................................................')
+    print(d.current_url)
+
+    WebDriverWait(d,15).until(EC.text_to_be_present_in_element((By.TAG_NAME,"table"),"Test Results"))
+    return d.find_element_by_tag_name('table').text
+ 
+ 
+
+
+
+
+
+
+# -------------------------------Routing------------------------------------
+@app.route('/',methods=['GET'])
+def ques_handle():
+    return getQues()
+ 
+@app.route('/ans',methods=['POST'])
+def ans_handle():
+    ans=request.get_json()
+    return getRes(ans)
+ 
+if __name__ == "__main__":
+    app.run(host="localhost", port=103)
+
+# -------------------------------Routing------------------------------------
+
+
+getQues()
